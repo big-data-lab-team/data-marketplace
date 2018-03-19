0bffb58e2c214f6c70f4e29b0b9ff63d3ec43955
Wassim Sabra <wassim_sabra@hotmail.com>
2018-03-19 00:55:03 -0400

    Mysql user update

--- controllers/mysql.js
+++ controllers/mysql.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 module.exports = mysql.createConnection({
     host:"localhost",
     port: '3306',
-    user:"root",
-    password:"...Database Password...",
+    user:"datacoin",
+    password:"datacoin",
     database: "datacoin",
     multipleStatements: true});
