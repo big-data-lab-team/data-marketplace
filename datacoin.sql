d52905a24468bc160bc88a4cd88b8b0cf0af66b0
Wassim Sabra <wassim_sabra@hotmail.com>
2018-03-26 21:47:03 -0400

    SQL file update

--- datacoin.sql
+++ datacoin.sql
@@ -35,7 +35,6 @@ FLUSH PRIVILEGES;
 
 -- --------------------------------------------------------
 
-
 --
 -- Table structure for table `categories`
 --
@@ -47,14 +46,16 @@ CREATE TABLE IF NOT EXISTS `categories` (
   `description` varchar(45) NOT NULL DEFAULT 'N/A',
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `categories`
 --
 
 INSERT INTO `categories` (`id`, `name`, `description`) VALUES
-(1, 'Location', 'User GPS location edited');
+(1, 'Location', 'User GPS location edited'),
+(3, 'Temperature', 'Temperature record from sensors'),
+(4, 'Other', 'Other non categorised data');
 
 -- --------------------------------------------------------
 
@@ -72,15 +73,29 @@ CREATE TABLE IF NOT EXISTS `data` (
   PRIMARY KEY (`id`,`owner_id`,`category_id`),
   KEY `fk_data_users_idx` (`owner_id`),
   KEY `fk_data_categories1_idx` (`category_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `data`
 --
 
 INSERT INTO `data` (`id`, `name`, `price`, `owner_id`, `category_id`) VALUES
-(1, 'MyLocation', '100', 1, 1),
-(2, 'Location Status', '20', 1, 1);
+(1, 'MyLocation', '100', 1, 3),
+(2, 'Location Status', '20', 2, 1),
+(3, 'Name1', '10', 3, 1),
+(4, 'Name2', '10', 3, 1),
+(5, 'Name3', '10', 3, 1),
+(6, 'Temperature', '10', 3, 1),
+(7, 'Temperature', '10', 3, 1),
+(8, 'Temperature', '10', 3, 1),
+(9, 'Temperature', '10', 3, 1),
+(10, 'Another Data', '10', 3, 1),
+(11, 'Dt', '10', 3, 1),
+(12, 'New Data', '102', 3, 1),
+(13, 'New2603', '105', 3, 1),
+(14, 'TestCategory', '10', 3, 4),
+(15, 'wassim', '10', 3, 1),
+(16, 'wassim', '10', 3, 1);
 
 -- --------------------------------------------------------
 
@@ -119,16 +134,22 @@ CREATE TABLE IF NOT EXISTS `tags` (
   `data_id` int(11) NOT NULL,
   PRIMARY KEY (`id`,`data_id`),
   KEY `fk_tags_data1_idx` (`data_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `tags`
 --
 
 INSERT INTO `tags` (`id`, `name`, `value`, `data_id`) VALUES
-(1, 'titleedited', 'some thing', 1),
+(1, 'titleedited', 'some thing', 2),
 (2, 'subtitleedited', 'something', 1),
-(3, 'undefined', 'SValue', 2);
+(3, 'undefined', 'SValue', 2),
+(4, 'undefined', 'undefined', 9),
+(5, 'undefined', 'undefined', 10),
+(6, 'undefined', 'undefined', 11),
+(7, 'For', 'Sale', 12),
+(8, 'Tag1', 'tg2', 13),
+(9, 'tag2', 'tg1', 12);
 
 -- --------------------------------------------------------
 
@@ -145,14 +166,25 @@ CREATE TABLE IF NOT EXISTS `transactions` (
   `data_id` int(11) NOT NULL,
   PRIMARY KEY (`id`,`data_id`),
   KEY `fk_transactions_data1_idx` (`data_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `transactions`
 --
 
 INSERT INTO `transactions` (`id`, `requester_uuid`, `status`, `timestamp`, `data_id`) VALUES
-(1, '35fee0fa-ddca-4392-a434-1edc53d25ab7', 'Data validated', 1521178978, 1);
+(1, '35fee0fa-ddca-4392-a434-1edc53d25ab7', 'Data validated', 1521178978, 1),
+(2, '2150ab55-5507-4af3-8651-6c8d3a1bdc28', 'Pending', 1522101549, 1),
+(3, '2150ab55-5507-4af3-8651-6c8d3a1bdc28', 'Pending', 1522102012, 1),
+(4, '2150ab55-5507-4af3-8651-6c8d3a1bdc28', 'Pending', 1522102041, 1),
+(5, '2150ab55-5507-4af3-8651-6c8d3a1bdc28', 'Pending', 1522102470, 1),
+(6, '2150ab55-5507-4af3-8651-6c8d3a1bdc28', 'Pending', 1522102557, 2),
+(7, '2150ab55-5507-4af3-8651-6c8d3a1bdc28', 'Pending', 1522102618, 1),
+(8, '2150ab55-5507-4af3-8651-6c8d3a1bdc28', 'Pending', 1522102660, 1),
+(9, '2150ab55-5507-4af3-8651-6c8d3a1bdc28', 'Pending', 1522102948, 1),
+(10, 'c6d8548b-21a2-4f34-a373-c7ad07e6bc2f', 'Pending', 1522103043, 1),
+(11, 'c6d8548b-21a2-4f34-a373-c7ad07e6bc2f', 'Pending', 1522103634, 1),
+(12, 'c6d8548b-21a2-4f34-a373-c7ad07e6bc2f', 'Pending', 1522106558, 1);
 
 -- --------------------------------------------------------
 
@@ -176,10 +208,11 @@ CREATE TABLE IF NOT EXISTS `users` (
   `validated` int(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`,`roles_id`),
   UNIQUE KEY `email_UNIQUE` (`email`),
+  UNIQUE KEY `username` (`username`),
   UNIQUE KEY `uuid_UNIQUE` (`uuid`),
   UNIQUE KEY `apiKey_UNIQUE` (`apiKey`),
   KEY `fk_users_roles1_idx` (`roles_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `users`
@@ -187,9 +220,15 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 INSERT INTO `users` (`id`, `uuid`, `username`, `password`, `apiKey`, `email`, `country`, `city`, `province`, `yearOfBirth`, `roles_id`, `validated`) VALUES
 (1, '5882989f-a4a8-4c94-8409-a68827b335d5', 'John', '81dc9bdb52d04dc20036dbd8313ed055', 'ZuIa7evHrLCFdgUo96lagg91cgPMcD', 'johnedited@smith.ca', 'Canada', 'Laval', 'Quebec', 1990, 1, 1),
-(2, '35fee0fa-ddca-4392-a434-1edc53d25ab7', 'Johnny', '81dc9bdb52d04dc20036dbd8313ed055', 'tRYyhRyHyJJf9zYlT0SA1dq5JaPDWO', 'john@smith.com', 'Canada', 'Montreal', 'Quebec', 1970, 1, 1),
-(3, 'c6d8548b-21a2-4f34-a373-c7ad07e6bc2f', 'datacoin', 'c78036d23335477b6b339e0b938d74a6', 'QFmzD0qIVCNwPs4dqDRPs8nQPLXLD8', 'john@datacoin.ca', 'Canada', 'Montreal', 'Quebec', 1992, 1, 0),
-(4, '2150ab55-5507-4af3-8651-6c8d3a1bdc28', 'tristan', 'c78036d23335477b6b339e0b938d74a6', 'fuQdssqjhUoyJ77ucsriTovMJP4dnL', 'john@datacoin1.ca', 'Canada', 'Montreal', 'Quebec', 1992, 1, 0);
+(2, '35fee0fa-ddca-4392-a434-1edc53d25ab7', 'Johnny', '81dc9bdb52d04dc20036dbd8313ed055', 'tRYyhRyHyJJf9zYlT0SA1dq5JaPDWO', 'john@smith.com', 'Canada', 'Montreal', 'Quebec', 1970, 3, 1),
+(3, 'c6d8548b-21a2-4f34-a373-c7ad07e6bc2f', 'datacoin', '979a73428d9179be44f3770b24d71dee', '9dMwqfvjZCu1lICsTJJjMDRgmXucvB', 'john@datacoin.ca', 'Canada', 'Montreal', 'Quebec', 1970, 1, 1),
+(4, '2150ab55-5507-4af3-8651-6c8d3a1bdc28', 'tristan', 'c78036d23335477b6b339e0b938d74a6', 'fuQdssqjhUoyJ77ucsriTovMJP4dnL', 'john@datacoin1.ca', 'Canada', 'Montreal', 'Quebec', 1992, 1, 0),
+(5, 'd0193cc1-3c09-43b6-8ee0-f316eab19159', 'wasim', '81dc9bdb52d04dc20036dbd8313ed055', 'tlG4ce4ekaHVs0FIKm6DGfiJBCcW83', 'wassim_sabra@hotmail.com', 'Canada', 'montreal', 'quebec', 1970, 1, 0),
+(6, '3f0d6aa9-a926-4880-849b-e6dbb85cfe6a', 'datacoin1', '979a73428d9179be44f3770b24d71dee', 'ofbKxGFhXt70iwJABrv5XEmtDJ3O7s', 'sabra1.entre1prise@live.com', 'Canada', 'montreal', 'quebec', 1970, 1, 0),
+(7, '7e20469a-1d8e-42dd-bf12-872a6544a93b', 'wass2', '979a73428d9179be44f3770b24d71dee', 'xCPwTznhaqcKmCERjzmQtSYc82L07B', 's_wer@hot.com', 'Canada', 'montreal', 'quebec', 1970, 1, 0),
+(8, '643d1ad5-29c3-40d0-bfb0-13110d617249', 'w_sabr', '7815696ecbf1c96e6894b779456d330e', 'NtBo7X6Nozc3Y7y6NnpNQn9BHfO3mf', 'sabra.entreprise@live.com', 'Canada', 'Saint-Laurent', 'QUEBEC', 1970, 1, 1),
+(9, '17497f51-27ee-4e0d-a36b-92c8dbbd1e78', 'w_sabra', '7815696ecbf1c96e6894b779456d330e', 'May2a6mzSkMisBFqgNj4WJX3mazd04', 'sabra.entrepris1e@live.ca', 'Canada', 'Saint-Laurent', 'QUEBEC', 1970, 1, 0),
+(10, '7ef4665b-7071-4582-b600-652639021d1a', 'w1_sabra', '7815696ecbf1c96e6894b779456d330e', 'tLp2kIMYqgqZGXqbJZxX2X1ycj39ac', 'sabra.entreprise@live.ca', 'Canada', 'Saint-Laurent', 'QUEBEC', 1970, 1, 0);
 
 --
 -- Constraints for dumped tables
@@ -199,7 +238,7 @@ INSERT INTO `users` (`id`, `uuid`, `username`, `password`, `apiKey`, `email`, `c
 -- Constraints for table `data`
 --
 ALTER TABLE `data`
-  ADD CONSTRAINT `fk_data_categories1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_data_categories1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_data_users` FOREIGN KEY (`owner_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
